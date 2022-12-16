8375ccb5fdff581b962dc2bfa720a579b8d8aba1
wmadwand <wmadwand@gmail.com>
2022-12-16 15:17:14 +0500

    RaceCamera follows Player X position

--- Assets/_Project/Scripts/Camera/RaceCamera.cs
+++ Assets/_Project/Scripts/Camera/RaceCamera.cs
@@ -6,6 +6,7 @@ public class RaceCamera : MonoBehaviour
 
     [SerializeField] private float _rotationSpeed = 50f;
     [SerializeField] private float _pitchRotationAngle = 15f;
+    [SerializeField] private float _followingHorizontalSpeed = 50f;
 
     private void Update()
     {
@@ -13,8 +14,10 @@ public class RaceCamera : MonoBehaviour
         var pitchRotation = Quaternion.Euler(_pitchRotationAngle, 0, 0);
         var targetRotation = Quaternion.LookRotation(playerRotation.SplineForward, Vector3.up) * pitchRotation;
         var resultRotation = Quaternion.RotateTowards(currentRotation, targetRotation, Time.deltaTime * _rotationSpeed);
-
         transform.rotation = resultRotation;
+
+        var xLerp = Mathf.Lerp(transform.position.x, playerRotation.transform.position.x, Time.deltaTime * _followingHorizontalSpeed);
+        transform.position = new Vector3(xLerp, transform.position.y, transform.position.z);
 
         Debug.DrawRay(transform.position, playerRotation.SplineForward * 1000, Color.magenta);
     }

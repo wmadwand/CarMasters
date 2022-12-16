3a93e7e9c79919d4f96ffa845959b92ee71fd310
wmadwand <wmadwand@gmail.com>
2022-12-16 14:51:28 +0500

    RaceCamera pitchRotation

--- Assets/_Project/Scripts/Camera/RaceCamera.cs
+++ Assets/_Project/Scripts/Camera/RaceCamera.cs
@@ -5,11 +5,13 @@ public class RaceCamera : MonoBehaviour
     public PlayerRotation playerRotation;
 
     [SerializeField] private float _rotationSpeed = 50f;
+    [SerializeField] private float _pitchRotationAngle = 15f;
 
     private void Update()
     {
         var currentRotation = transform.rotation;
-        var targetRotation = Quaternion.LookRotation(playerRotation.SplineForward, Vector3.up);
+        var pitchRotation = Quaternion.Euler(_pitchRotationAngle, 0, 0);
+        var targetRotation = Quaternion.LookRotation(playerRotation.SplineForward, Vector3.up) * pitchRotation;
         var resultRotation = Quaternion.RotateTowards(currentRotation, targetRotation, Time.deltaTime * _rotationSpeed);
 
         transform.rotation = resultRotation;

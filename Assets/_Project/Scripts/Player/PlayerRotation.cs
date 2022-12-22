334d916cbf0680fc9d74a11da2b8526084bdba9a
wmadwand <wmadwand@gmail.com>
2022-12-22 16:29:32 +0500

    Minor changes

--- Assets/_Project/Scripts/Player/PlayerRotation.cs
+++ Assets/_Project/Scripts/Player/PlayerRotation.cs
@@ -6,17 +6,17 @@ public class PlayerRotation : MonoBehaviour
 {
     public Image rotationDetector;
 
-    [SerializeField] private float _rotationSpeed = 1;
-    [SerializeField] private float _rotationSmooth = 10;
-    [SerializeField] private float _rotationValuableRate = 3;
-    [SerializeField] private float _rotationToSplineSpeedAutopilot = 5;
-    [SerializeField] private float _rotationToSplineSpeedAfterManulaTurn = 5;
-    [SerializeField] private bool _shouldLookAlongSplineForward = true;
+    [SerializeField] private float _speed = 1;
+    [SerializeField] private float _smooth = 10;
+    [SerializeField] private float _sensitivity = 3;
+    [SerializeField] private float _autoRotationToSplineSpeed = 5;
+    [SerializeField] private float _manualRotationToSplineSpeed = 5;
+    [SerializeField] private bool _useAutoRotationToSpline = true;
 
     private float _currentSpeed = 0;
     private Rigidbody _rigidbody = null;
     private SplineProjector _splineProjector = null;
-    private float xInput = 0;
+    private float _xInput = 0;
     private bool _isMoveButtonPressed = false;
     private bool _isRotatingByUser = false;
 
@@ -26,7 +26,7 @@ public class PlayerRotation : MonoBehaviour
 
     public void SetXInput(float value)
     {
-        xInput = value;
+        _xInput = value;
     }
 
     public void SetMove(bool isActive)
@@ -38,7 +38,7 @@ public class PlayerRotation : MonoBehaviour
 
     private void Start()
     {
-        _currentSpeed = _rotationToSplineSpeedAutopilot;
+        _currentSpeed = _autoRotationToSplineSpeed;
     }
 
     private void Awake()
@@ -49,45 +49,43 @@ public class PlayerRotation : MonoBehaviour
 
     private void Update()
     {
-        //xInput = Input.GetAxis("Mouse X");
-        var inputAngleRotation = xInput * _rotationSpeed;
+        var inputAngleRotation = _xInput * _speed;
 
-        if (Mathf.Abs(inputAngleRotation) > _rotationValuableRate /*&& _isMoveButtonPressed*/)
+        if (Mathf.Abs(inputAngleRotation) > _sensitivity)
         {
             _isRotatingByUser = true;
-            _currentSpeed = _rotationToSplineSpeedAfterManulaTurn;
+            _currentSpeed = _manualRotationToSplineSpeed;
             rotationDetector.color = Color.green;
         }
-        else if (Mathf.Abs(inputAngleRotation) < _rotationValuableRate /*|| !_isMoveButtonPressed*/)
+        else if (Mathf.Abs(inputAngleRotation) < _sensitivity)
         {
             _isRotatingByUser = false;
-            //_currentSpeed = _rotationToSplineSpeedAutopilot;
             rotationDetector.color = Color.red;
 
-            xInput = 0;
+            _xInput = 0;
         }
         else if (!_isMoveButtonPressed)
         {
             _isRotatingByUser = false;
-            _currentSpeed = _rotationToSplineSpeedAutopilot;
+            _currentSpeed = _autoRotationToSplineSpeed;
 
-            xInput = 0;
+            _xInput = 0;
         }
 
         if (!_isMoveButtonPressed)
         {
-            xInput = 0;
+            _xInput = 0;
         }
 
         if (_isMoveButtonPressed)
         {
             if (_isRotatingByUser)
             {
-                ManualRotation(xInput);
+                ManualRotation(_xInput);
             }
             else
             {
-                if (_shouldLookAlongSplineForward)
+                if (_useAutoRotationToSpline)
                 {
                     AutoRotation(_currentSpeed);
                 }
@@ -95,16 +93,15 @@ public class PlayerRotation : MonoBehaviour
         }
         else if (!_isMoveButtonPressed && GetComponent<PlayerMovement>().IsMoving)
         {
-            if (_shouldLookAlongSplineForward)
+            if (_useAutoRotationToSpline)
             {
                 AutoRotation(_currentSpeed);
             }
         }
 
         if (Vector3.Dot(transform.forward, _splineProjector.result.forward) == 1 && !_isRotatingByUser)
-        {
-            //_isRotatingByUser = false;
-            _currentSpeed = _rotationToSplineSpeedAutopilot;
+        {            
+            _currentSpeed = _autoRotationToSplineSpeed;
             rotationDetector.color = Color.red;
         }
 
@@ -114,9 +111,9 @@ public class PlayerRotation : MonoBehaviour
 
     private void ManualRotation(float angle)
     {
-        angle *= _rotationSpeed;
+        angle *= _speed;
         var targetRot = _rigidbody.rotation * Quaternion.AngleAxis(angle, transform.up);
-        var res = Quaternion.Slerp(_rigidbody.rotation, targetRot, Time.deltaTime * _rotationSmooth);
+        var res = Quaternion.Slerp(_rigidbody.rotation, targetRot, Time.deltaTime * _smooth);
 
         _rigidbody.MoveRotation(res);
     }

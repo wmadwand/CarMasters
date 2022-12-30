97a776c8dc007c596b910130b03a0c693361b432
wmadwand <wmadwand@gmail.com>
2022-12-30 12:23:52 +0500

    PlayerRotationNew manual rotation in a local space

--- Assets/PlayerRotationNew.cs
+++ Assets/PlayerRotationNew.cs
@@ -5,7 +5,7 @@ using UnityEngine.UI;
 public class PlayerRotationNew : MonoBehaviour
 {
     public Image rotationDetector;
-    public bool useAutorotationToForward = false;
+    public Transform localRotator;
 
     [SerializeField] private float _speed = 1;
     [SerializeField] private float _smooth = 10;
@@ -16,12 +16,10 @@ public class PlayerRotationNew : MonoBehaviour
 
     [SerializeField] private float _disatnceAfterManualTurn = 2;
 
-    private float _currentSpeed = 0;
     private Rigidbody _rigidbody = null;
     private SplineProjector _splineProjector = null;
     private float _xInput = 0;
     private bool _isMoveButtonPressed = false;
-    private bool _isRotatingByUser = false;
 
     //---------------------------------------------------------------
 
@@ -38,11 +36,6 @@ public class PlayerRotationNew : MonoBehaviour
     }
 
     //---------------------------------------------------------------
-
-    private void Start()
-    {
-        _currentSpeed = _autoRotationToSplineSpeed;
-    }
 
     private void Awake()
     {
@@ -67,12 +60,16 @@ public class PlayerRotationNew : MonoBehaviour
                 isManualTurnFinished = false;
 
                 var angleClamp = Mathf.Clamp(_inputAngleRotation, -45, 45);
-
-                var startRot = _rigidbody.rotation;
-                var targetRot = Quaternion.AngleAxis(angleClamp, transform.up);
+                var startRot = localRotator.localRotation;
+                var targetRot = Quaternion.AngleAxis(angleClamp, localRotator.transform.up);
                 var resRot = Quaternion.RotateTowards(startRot, targetRot, Time.deltaTime * _smooth);
 
-                _rigidbody.MoveRotation(resRot);
+                localRotator.localRotation = resRot;
+                _splineProjector.motion.rotationOffset = localRotator.localRotation.eulerAngles;
+
+                //var offset = _splineProjector.motion.rotationOffset;
+                //var yy = Mathf.Lerp(offset.y, angleClamp, Time.deltaTime * _smooth);
+                //_splineProjector.motion.rotationOffset = new Vector3(offset.x, yy, offset.z);
 
                 var checkAngle = Quaternion.Angle(startRot, targetRot);
                 Debug.Log($"checkAngle = {checkAngle}");
@@ -83,7 +80,7 @@ public class PlayerRotationNew : MonoBehaviour
                     _xInput = 0;
 
                     isManualTurnFinished = true;
-                    startPosAfterManualTurnFinished = transform.position;
+                    startPosAfterManualTurnFinished = localRotator.position;
                 }
 
                 Debug.Log($"_inputAngleRotation = {angleClamp}");
@@ -91,14 +88,14 @@ public class PlayerRotationNew : MonoBehaviour
 
             if (isManualTurnFinished)
             {
-                if (Vector3.Distance(startPosAfterManualTurnFinished, transform.position) >= _disatnceAfterManualTurn)
+                if (Vector3.Distance(startPosAfterManualTurnFinished, localRotator.position) >= _disatnceAfterManualTurn)
                 {
                     isAutoTurning = true;
 
-
+                    var startRot = localRotator.localRotation;
                     AutoRotationNew(_manualRotationToSplineSpeed, out Quaternion targetRotation);
 
-                    var checkThatAngle = Quaternion.Angle(_rigidbody.rotation, targetRotation);
+                    var checkThatAngle = Quaternion.Angle(startRot, targetRotation);
                     Debug.Log($"checkThatAngle = {checkThatAngle}");
 
                     if (checkThatAngle <= 0)
@@ -111,7 +108,7 @@ public class PlayerRotationNew : MonoBehaviour
 
             if (_useAutoRotationToSpline && _inputAngleRotation == 0 && !isManualTurnFinished && !isAutoTurning)
             {
-                AutoRotationNew(_autoRotationToSplineSpeed, out Quaternion targetRotation);
+                //AutoRotationNew(_autoRotationToSplineSpeed, out Quaternion targetRotation);
             }
         }
     }
@@ -119,45 +116,50 @@ public class PlayerRotationNew : MonoBehaviour
 
     private void AutoRotationNew(float speed, out Quaternion targetRottt)
     {
-        var playerForward = transform.forward;
+        //var playerForward = transform.forward;
 
-        var splineForward = Vector3.forward;
-        if (_splineProjector != null)
-        {
-            splineForward = _splineProjector.result.forward;
-        }
+        //var splineForward = Vector3.forward;
+        //if (_splineProjector != null)
+        //{
+        //    splineForward = _splineProjector.result.forward;
+        //}
 
-        var targetRot = _rigidbody.rotation * Quaternion.FromToRotation(playerForward, splineForward);
+        //var targetRot = Quaternion. LookRotation(Quaternion.identity.fo, transform.up);
+        //var targetRot = Quaternion.identity;
 
-        // OR LOOK ROTATION !!!
-        //targetRot = Quaternion.Slerp(_rigidbody.rotation, targetRot, Time.deltaTime * _manualRotationToSplineSpeed);
+        //var resRot = Quaternion.RotateTowards(localRotator.rotation, targetRot, Time.deltaTime * speed);
+        var targetRot = Quaternion.Euler(0, 0, 0);
+        // TODO: Better use RotateTowards
+        var resRot = Quaternion.Lerp(localRotator.localRotation, targetRot, Time.deltaTime * speed);
 
-        var resRot = Quaternion.RotateTowards(_rigidbody.rotation, targetRot, Time.deltaTime * speed /*_manualRotationToSplineSpeed*/);
 
-        _rigidbody.MoveRotation(resRot);
+        localRotator.localRotation = resRot;
+        _splineProjector.motion.rotationOffset = localRotator.localRotation.eulerAngles;
+
+        //_rigidbody.MoveRotation(resRot);
 
         targetRottt = targetRot;
     }
 
 
-    private void ManualRotation(float angle)
-    {
-        angle *= _speed;
-        var targetRot = _rigidbody.rotation * Quaternion.AngleAxis(angle, transform.up);
-        var res = Quaternion.Slerp(_rigidbody.rotation, targetRot, Time.deltaTime * _smooth);
+    //private void ManualRotation(float angle)
+    //{
+    //    angle *= _speed;
+    //    var targetRot = _rigidbody.rotation * Quaternion.AngleAxis(angle, transform.up);
+    //    var res = Quaternion.Slerp(_rigidbody.rotation, targetRot, Time.deltaTime * _smooth);
 
-        _rigidbody.MoveRotation(res);
-    }
+    //    _rigidbody.MoveRotation(res);
+    //}
 
-    private void AutoRotation(float speed)
-    {
-        var playerForward = transform.forward;
-        var splineForward = _splineProjector.result.forward;
-        var targetRot = _rigidbody.rotation * Quaternion.FromToRotation(playerForward, splineForward);
-        targetRot = Quaternion.Slerp(_rigidbody.rotation, targetRot, Time.deltaTime * speed);
+    //private void AutoRotation(float speed)
+    //{
+    //    var playerForward = transform.forward;
+    //    var splineForward = _splineProjector.result.forward;
+    //    var targetRot = _rigidbody.rotation * Quaternion.FromToRotation(playerForward, splineForward);
+    //    targetRot = Quaternion.Slerp(_rigidbody.rotation, targetRot, Time.deltaTime * speed);
 
-        _rigidbody.MoveRotation(targetRot);
-    }
+    //    _rigidbody.MoveRotation(targetRot);
+    //}
 
     //private void Update_old()
     //{
